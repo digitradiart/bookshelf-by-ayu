dfd527cdcb23cde80fa7ce053233ce5f75621e6d
digitradiart <digitradiart@gmail.com>
2021-07-24 12:02:47 +0700

    dom

--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,48 @@
-const UNCOMPLETED_LIST_TODO_ID = "todos";
-const COMPLETED_LIST_TODO_ID = "completed-todos";
+/* Kriteria 1: Mampu Menambahkan Data Buku
+{
+    isbn: number,
+    title: string,
+    author: string,
+    year: number,
+    date: date,
+    isCompleted: boolean,
+  }
+*/
+
+const UNCOMPLETED_LIST_BOOK_ID = "unreadBooks";
+const COMPLETED_LIST_BOOK_ID = "readBooks";
 const TODO_ITEMID = "itemId";
 
-function makeTodo(data /* string */ , timestamp /* string */ , isCompleted /* boolean */ ) {
+// Menampilkan Item Buku dan Menandai Buku Selesai
+function makeBooks(inputISBN, inputTitle, inputAuthor, inputYear, inputDate, isCompleted) {
 
-    const textTitle = document.createElement("h2");
-    textTitle.innerText = data;
+    const textInputISBN = document.createElement("h1");
+    // textInputISBN.innerText = `ISBN: ${inputISBN}`;
+    textInputISBN.innerText = `${inputISBN}`;
 
-    const textTimestamp = document.createElement("p");
-    textTimestamp.innerText = timestamp;
+    const textInputTitle = document.createElement("h2");
+    // textInputTitle.innerText = `Title: ${inputTitle}`;
+    textInputTitle.innerText = `${inputTitle}`;
+
+    const textInputAuthor = document.createElement("h3");
+    // textInputAuthor.innerText = `Author: ${inputAuthor}`;
+    textInputAuthor.innerText = `${inputAuthor}`;
+
+    const textInputYear = document.createElement("h4");
+    // textInputYear.innerText = `Book's Year: ${inputYear}`;
+    textInputYear.innerText = `${inputYear}`;
+
+    const textInputDate = document.createElement("p");
+    // textInputDate.innerText = `Date: ${inputDate}`;
+    textInputDate.innerText = `${inputDate}`;
 
     const textContainer = document.createElement("div");
     textContainer.classList.add("inner")
-    textContainer.append(textTitle, textTimestamp);
+    textContainer.append(textInputISBN, textInputTitle, textInputAuthor, textInputYear, textInputDate);
+
 
     const container = document.createElement("div");
-    container.classList.add("item", "shadow")
+    container.classList.add("item", "shadow");
     container.append(textContainer);
 
     if (isCompleted) {
@@ -28,10 +55,27 @@ function makeTodo(data /* string */ , timestamp /* string */ , isCompleted /* bo
             createCheckButton()
         );
     }
-
     return container;
 }
 
+/*
+Kriteria 2: Memiliki Dua Rak Buku
+Bookshelf Apps harus memiliki 2 Rak buku. Yakni, “Belum selesai dibaca” dan “Selesai dibaca”.
+Rak buku Belum selesai dibaca hanya menyimpan buku yang properti isCompleted nya bernilai false.
+Rak buku Selesai dibaca hanya menyimpan buku yang properti isCompleted nya bernilai true.
+*/
+
+/*
+Kriteria 3: Dapat Memindahkan Buku antar Rak
+Buku yang ditampilkan pada rak, baik itu "Belum selesai dibaca" maupun "Selesai dibaca" harus dapat dipindahkan di antara keduanya.
+*/
+
+/*
+Kriteria 4: Dapat Menghapus Data Buku
+Buku yang ditampilkan pada rak baik itu "Belum selesai dibaca" maupun "Selesai dibaca" harus dapat dihapus.
+*/
+
+// createButton
 function createUndoButton() {
     return createButton("undo-button", function(event) {
         undoTaskFromCompleted(event.target.parentElement);
@@ -60,29 +104,37 @@ function createButton(buttonTypeClass /* string */ , eventListener /* Event */ )
     return button;
 }
 
-function addTodo() {
-    const uncompletedTODOList = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
-    const textTodo = document.getElementById("title").value;
-    const timestamp = document.getElementById("date").value;
+function addBooks() {
+    const uncompletedBOOKList = document.getElementById(UNCOMPLETED_LIST_BOOK_ID);
 
-    const todo = makeTodo(textTodo, timestamp, false);
-    const todoObject = composeTodoObject(textTodo, timestamp, false);
+    const inputISBN = document.getElementById("isbn").value;
+    const inputTitle = document.getElementById("title").value;
+    const inputAuthor = document.getElementById("author").value;
+    const inputYear = document.getElementById("year").value;
+    const inputDate = document.getElementById("date").value;
+
+    const todo = makeBooks(inputISBN, inputTitle, inputAuthor, inputYear, inputDate, false);
+    const todoObject = composeTodoObject(inputISBN, inputTitle, inputAuthor, inputYear, inputDate, false);
 
     todo[TODO_ITEMID] = todoObject.id;
     todos.push(todoObject);
 
-    uncompletedTODOList.append(todo);
+    uncompletedBOOKList.append(todo);
     updateDataToStorage();
 }
 
-function addTaskToCompleted(taskElement /* HTMLELement */ ) {
-    const listCompleted = document.getElementById(COMPLETED_LIST_TODO_ID);
 
+function addTaskToCompleted(taskElement) {
+    const listCompleted = document.getElementById(COMPLETED_LIST_BOOK_ID);
+
+    const taskISBN = taskElement.querySelector(".inner > h1").innerText;
     const taskTitle = taskElement.querySelector(".inner > h2").innerText;
-    const taskTimestamp = taskElement.querySelector(".inner > p").innerText;
+    const taskAuthor = taskElement.querySelector(".inner > h3").innerText;
+    const taskYear = taskElement.querySelector(".inner > h4").innerText;
+    const taskDate = taskElement.querySelector(".inner > p").innerText;
 
-    const newTodo = makeTodo(taskTitle, taskTimestamp, true);
 
+    const newTodo = makeBooks(taskISBN, taskTitle, taskAuthor, taskYear, taskDate, true);
 
     const todo = findTodo(taskElement[TODO_ITEMID]);
     todo.isCompleted = true;
@@ -103,13 +155,17 @@ function removeTaskFromCompleted(taskElement /* HTMLELement */ ) {
     updateDataToStorage();
 }
 
-function undoTaskFromCompleted(taskElement /* HTMLELement */ ) {
-    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
+function undoTaskFromCompleted(taskElement) {
+    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_BOOK_ID);
 
+    const taskISBN = taskElement.querySelector(".inner > h1").innerText;
     const taskTitle = taskElement.querySelector(".inner > h2").innerText;
-    const taskTimestamp = taskElement.querySelector(".inner > p").innerText;
+    const taskAuthor = taskElement.querySelector(".inner > h3").innerText;
+    const taskYear = taskElement.querySelector(".inner > h4").innerText;
+    const taskDate = taskElement.querySelector(".inner > p").innerText;
 
-    const newTodo = makeTodo(taskTitle, taskTimestamp, false);
+
+    const newTodo = makeBooks(taskISBN, taskTitle, taskAuthor, taskYear, taskDate, false);
 
     const todo = findTodo(taskElement[TODO_ITEMID]);
     todo.isCompleted = false;
@@ -119,15 +175,25 @@ function undoTaskFromCompleted(taskElement /* HTMLELement */ ) {
     taskElement.remove();
 
     updateDataToStorage();
+
 }
 
-// refresh data form Todos
+
+/*
+Kriteria 5: Manfaatkan localStorage dalam Menyimpan Data Buku
+Data buku yang ditampilkan pada rak, baik itu "Belum selesai dibaca" maupun "Selesai dibaca" harus dapat bertahan walaupun halaman web ditutup.
+Dengan begitu, Anda harus menyimpan data buku pada localStorage.
+*/
+
 function refreshDataFromTodos() {
-    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
-    let listCompleted = document.getElementById(COMPLETED_LIST_TODO_ID);
+    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_BOOK_ID);
+    let listCompleted = document.getElementById(COMPLETED_LIST_BOOK_ID);
 
     for (todo of todos) {
-        const newTodo = makeTodo(todo.task, todo.timestamp, todo.isCompleted);
+        // const newTodo = makeBooks(todo.task, todo.timestamp, todo.isCompleted);
+        const newTodo = makeBooks(todo.isbn, todo.title, todo.author, todo.year, todo.date, todo.isCompleted);
+
+
         newTodo[TODO_ITEMID] = todo.id;
 
         if (todo.isCompleted) {
